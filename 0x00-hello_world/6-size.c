c093b53a84b0d7eea4a5308bd64d661f9b496d56
Alaa3Eldin <alaa3eldeen@gmail.com>
2024-05-02 14:19:49 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,5 @@ printf("Size of int: %zu bytes\n", sizeof(int));
 printf("Size of long: %zu bytes\n", sizeof(long int ));
 printf("Size of long long: %zu bytes\n", sizeof(long long int ));
 printf("Size of float: %zu bytes\n", sizeof(float));
-printf("Size of pointer: %zu bytes\n", sizeof(void *));
 return 0;
 }
